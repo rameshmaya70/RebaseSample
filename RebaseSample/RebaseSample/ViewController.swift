2d46c9ed4f314237176891634898294a9a4ec512
rameshmaya70 <ramesh.khanna@altimetrik.com>
2016-01-29 11:50:49 +0530

    Initial commit

---
+++ RebaseSample/RebaseSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RebaseSample
+//
+//  Created by Ramesh Khanna on 1/29/16.
+//  Copyright © 2016 Ramesh Khanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
